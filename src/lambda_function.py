42dae9a9af58bcf60f00d04d1d5d26e26b61d1d1
jzhu4 <jzhu4@alaska.edu>
2022-04-14 20:41:34 -0800

    modify function

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -1,8 +1,4 @@
-import json
-
 def lambda_handler(event, context):
-    # TODO implement
     return {
-        'message': 'Hello world {}'.format(event['name']), 
-        'body': json.dumps('Hello from Lambda!')
+        'message': 'Hello world {}'.format(event['name'])
     }
